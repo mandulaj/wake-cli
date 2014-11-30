acd66936594d729c3332da425654755e2f8e78e7
Jakub Mandula <jakub.aludnam@gmail.com>
2014-11-30 20:01:34 +0100

    add tests

--- test/test.js
+++ test/test.js
@@ -18,11 +18,10 @@ function clone(obj) {
 // TODO: add more tests!
 
 describe("DataGetter", function() {
+  var conf = clone(config);
+  conf.wakefile = "./test/testWakefiles/wakefile1.json";
+  var dg = require("../lib/dataGetter.js")(conf, false);
   describe("#init", function() {
-    var conf = clone(config);
-    conf.wakefile = "./test/testWakefiles/wakefile1.json";
-    var dg = require("../lib/dataGetter.js")(conf, false);
-
     it("should return and object", function() {
       expect(dg).to.be.an('object');
       expect(dg.file).to.be.a('string');
@@ -34,5 +33,28 @@ describe("DataGetter", function() {
     it('should set the path to be based in the home directory', function() {
       // TODO: test for the correct homedir concat
     });
+    it('should have all functions', function() {
+      expect(dg.fixOldFormat).to.be.a("function");
+      expect(dg.reparseData).to.be.a("function");
+      expect(dg.getItems).to.be.a("function");
+      expect(dg.getItem).to.be.a("function");
+      expect(dg.indexOfDev).to.be.a("function");
+      expect(dg.deviceExists).to.be.a("function");
+      expect(dg.addItem).to.be.a("function");
+      expect(dg.removeItem).to.be.a("function");
+      expect(dg.updateItemTime).to.be.a("function");
+      expect(dg.listSaved).to.be.a("function");
+      expect(dg.save).to.be.a("function");
+    });
+  });
+  describe("#getItems", function() {
+    var macs = dg.getItems();
+    it("should return and array", function() {
+      expect(macs).to.be.an("array");
+      expect(macs).to.have.length(0);
+    });
+  });
+  describe("#getItem", function() {
+
   });
 });
