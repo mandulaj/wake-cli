849ccfeb8894fde771d074d4b388568ac5ce8d22
Jakub Mandula <jakub.aludnam@gmail.com>
2014-12-03 23:32:40 +0100

    rewrite datageter function + add tests

--- test/test.js
+++ test/test.js
@@ -18,159 +18,183 @@ function clone(obj) {
 // TODO: add more tests!
 
 describe("Helper Functions", function() {
-  describe("DataGetter", function() {
-    var conf = clone(config);
-    conf.wakefile = "./test/testWakefiles/wakefile1.json";
-    var dg = require("../lib/dataGetter.js")(conf, ".");
-    dg.getDataFromFile();
-    describe("#init", function() {
-      it("should return and object", function() {
-        expect(dg).to.be.an('object');
-        expect(dg.filePath).to.be.a('string');
-        expect(dg.data).to.be.an('object');
-      });
-      it('should override the default path', function() {
-        expect(dg.filePath).to.be("test/testWakefiles/wakefile1.json");
-
-      });
-      it('should set the path to be based in the home directory', function() {
-        // TODO: test for the correct homedir concat
-      });
-      it('should have all functions', function() {
-        expect(dg.fixOldFormat).to.be.a("function");
-        expect(dg.serialize).to.be.a("function");
-        expect(dg.getItems).to.be.a("function");
-        expect(dg.getItemByName).to.be.a("function");
-        expect(dg.indexOfDev).to.be.a("function");
-        expect(dg.deviceExists).to.be.a("function");
-        expect(dg.addItem).to.be.a("function");
-        expect(dg.removeItem).to.be.a("function");
-        expect(dg.updateItemTime).to.be.a("function");
-        expect(dg.save).to.be.a("function");
-        expect(dg.buildPath).to.be.a("function");
-      });
-    });
-    describe("#getItems", function() {
-      var macs = dg.getItems();
-      it("should return and array", function() {
-        expect(macs).to.be.an("array");
-        expect(macs).to.have.length(0);
-      });
-      it("should return all the saved elements", function(){
-        dg.addItem({name:"a"});
-        dg.addItem({name:"b"});
-        dg.addItem({name:"c"});
-        dg.addItem({name:"d"});
-        expect(dg.getItems()).to.have.length(4);
-        dg.removeItem("a");
-        expect(dg.getItems()).to.have.length(3);
-        dg.removeItem("c");
-        expect(dg.getItems()).to.have.length(2);
-        dg.removeItem("b");
-        expect(dg.getItems()).to.have.length(1);
-        dg.removeItem("d");
-        expect(dg.getItems()).to.have.length(0);
-      });
-    });
-    describe("#buildPath", function() {
-      it("should return a string path", function(){
-        expect(dg.buildPath("test")).to.be.a("string");
-      });
-      it("should join the base-path with the path from the config file", function(){
-        var path = dg.buildPath("/home/user");
-        expect(path).to.be("/home/user/test/testWakefiles/wakefile1.json");
-        path = dg.buildPath("../../");
-        expect(path).to.be("../../test/testWakefiles/wakefile1.json");
-        path = dg.buildPath("/");
-        expect(path).to.be("/test/testWakefiles/wakefile1.json");
-        path = dg.buildPath("./");
-        expect(path).to.be("test/testWakefiles/wakefile1.json");
-      });
-    });
-    describe("#getDataFromFile", function() {
-      it("should read data form the file at the given path", function(){
-        var conf = clone(config);
-        conf.wakefile = "test/testWakefiles/wakefile2.json";
-        var datget = require("../lib/dataGetter.js")(conf, ".");
-        datget.getDataFromFile();
-        var items = datget.getItems();
-        expect(items).to.have.length(0);
-      });
-    });
-    describe("#makeNewData", function() {
-      it("should return and object with default values", function(){
-        var data = dg.makeNewData();
-        expect(data).to.be.an("object");
-        expect(data.version).to.be(conf.version);
-        expect(data.saved_macs).to.be.an("array");
-        expect(data.saved_macs).to.have.length(0);
-      });
-      it("should ignore false input", function(){
-        var data = dg.makeNewData("straw");
-        expect(data).to.be.an("object");
-        expect(data.version).to.be(conf.version);
-        expect(data.saved_macs).to.be.an("array");
-        expect(data.saved_macs).to.have.length(0);
-        data = dg.makeNewData(true);
-        expect(data).to.be.an("object");
-        expect(data.version).to.be(conf.version);
-        expect(data.saved_macs).to.be.an("array");
-        expect(data.saved_macs).to.have.length(0);
-      });
-      it("should be able to set custom initial objects", function(){
-        var data = dg.makeNewData({
-          key1: [1,2,3,4,5],
-          key2: true,
-          key3: "testing"
+  var conf = clone(config);
+  conf.wakefile = "./test/testWakefiles/wakefile1.json";
+  var dg = require("../lib/dataGetter.js")(conf, ".");
+  dg.getDataFromFile(function(err, data){
+    if (err) expect().fail("Can't get data", err);
+    describe("DataGetter", function() {
+      describe("#init", function() {
+        it("should return and object", function() {
+          expect(dg).to.be.an('object');
+          expect(dg.filePath).to.be.a('string');
+          expect(dg.data).to.be.an('object');
+          expect(dg.config).to.eql(conf);
         });
-        expect(data).to.be.an("object");
-        expect(data.key1).to.eql([1,2,3,4,5]);
-        expect(data.key2).to.be(true);
-        expect(data.key3).to.be("testing");
+        it('should use the provided base path', function() {
+          expect(dg.filePath).to.be("test/testWakefiles/wakefile1.json");
+        });
+        it('should used the default configuration when config null is provided', function(){
+          var dg3 = require("../lib/dataGetter.js")(null, ".");
+          expect(dg3.config).to.eql(config); // default config
+          delete dg3;
+        });
+        it('should used the default configuration when config false is provided', function(){
+          var dg3 = require("../lib/dataGetter.js")(false, ".");
+          expect(dg3.config).to.eql(config); // default config
+          delete dg3;
+        });
+        it('should used the default configuration when config undefined is provided', function(){
+          var dg3 = require("../lib/dataGetter.js")(undefined, ".");
+          expect(dg3.config).to.eql(config); // default config
+          delete dg3;
+        });
+        it('should have all functions', function() {
+          expect(dg.fixOldFormat).to.be.a("function");
+          expect(dg.serialize).to.be.a("function");
+          expect(dg.getItems).to.be.a("function");
+          expect(dg.getItemByName).to.be.a("function");
+          expect(dg.indexOfDev).to.be.a("function");
+          expect(dg.deviceExists).to.be.a("function");
+          expect(dg.addItem).to.be.a("function");
+          expect(dg.removeItem).to.be.a("function");
+          expect(dg.updateItemTime).to.be.a("function");
+          expect(dg.save).to.be.a("function");
+          expect(dg.buildPath).to.be.a("function");
+        });
       });
-    });
-    describe("#parseFileData", function() {
-      it("should return an object", function(){
-        var data = dg.parseFileData('{"name": "test", "saved_macs": [] }');
-        expect(data).to.be.an("object");
-        expect(data).to.eql({name: "test", saved_macs: []});
+      describe("#getItems", function() {
+        var macs = dg.getItems();
+        it("should return and array", function() {
+          expect(macs).to.be.an("array");
+          expect(macs).to.have.length(0);
+        });
+        it("should return all the saved elements", function(){
+          dg.addItem({name:"a"});
+          dg.addItem({name:"b"});
+          dg.addItem({name:"c"});
+          dg.addItem({name:"d"});
+          expect(dg.getItems()).to.have.length(4);
+          dg.removeItem("a");
+          expect(dg.getItems()).to.have.length(3);
+          dg.removeItem("c");
+          expect(dg.getItems()).to.have.length(2);
+          dg.removeItem("b");
+          expect(dg.getItems()).to.have.length(1);
+          dg.removeItem("d");
+          expect(dg.getItems()).to.have.length(0);
+        });
       });
-      it("should gracefully return null on bad input", function(){
-        var data = dg.parseFileData("{'name' 'test', 'saved_macs': []}");
-        expect(data).to.be(null);
+      describe("#buildPath", function() {
+        it("should return a string path", function(){
+          expect(dg.buildPath("test")).to.be.a("string");
+        });
+        it("should join the base-path with the path from the config file", function(){
+          var path = dg.buildPath("/home/user");
+          expect(path).to.be("/home/user/test/testWakefiles/wakefile1.json");
+          path = dg.buildPath("../../");
+          expect(path).to.be("../../test/testWakefiles/wakefile1.json");
+          path = dg.buildPath("/");
+          expect(path).to.be("/test/testWakefiles/wakefile1.json");
+          path = dg.buildPath("./");
+          expect(path).to.be("test/testWakefiles/wakefile1.json");
+        });
       });
-    });
-    describe("#fixOldFormat", function() {
-
-    });
-    describe("#serialize", function() {
-
-    });
-    describe("#indexOfDev", function() {
-
-    });
-    describe("#deviceExists", function() {
-
-    });
-    describe("#addItem", function() {
-      var macs = dg.getItems();
-      expect(macs).to.have.length(0);
-      dg.addItem({
-        name: "test"
+      describe("#getDataFromFile", function() {
+        it("should read data form the file at the given path", function(done){
+          var conf = clone(config);
+          conf.wakefile = "test/testWakefiles/wakefile2.json";
+          var datget = require("../lib/dataGetter.js")(conf, ".");
+          datget.getDataFromFile(function(err, data){
+            if (err)
+              expect().fail("Error while getting data: ", err);
+            var items = datget.getItems();
+            expect(items).to.have.length(0);
+            done();
+          }, true);
+        });
       });
-      expect(dg.getItemByName("test")).to.be.an("object");
-      macs = dg.getItems();
-      expect(macs).to.have.length(1);
-      dg.removeItem("test");
-    });
-    describe("#removeItem", function() {
+      describe("#makeNewData", function() {
+        it("should return and object with default values", function(){
+          var data = dg.makeNewData();
+          expect(data).to.be.an("object");
+          expect(data.version).to.be(conf.version);
+          expect(data.saved_macs).to.be.an("array");
+          expect(data.saved_macs).to.have.length(0);
+        });
+        it("should ignore false input", function(){
+          var data = dg.makeNewData("straw");
+          expect(data).to.be.an("object");
+          expect(data.version).to.be(conf.version);
+          expect(data.saved_macs).to.be.an("array");
+          expect(data.saved_macs).to.have.length(0);
+          data = dg.makeNewData(true);
+          expect(data).to.be.an("object");
+          expect(data.version).to.be(conf.version);
+          expect(data.saved_macs).to.be.an("array");
+          expect(data.saved_macs).to.have.length(0);
+        });
+        it("should be able to set custom initial objects", function(){
+          var data = dg.makeNewData({
+            key1: [1,2,3,4,5],
+            key2: true,
+            key3: "testing"
+          });
+          expect(data).to.be.an("object");
+          expect(data.key1).to.eql([1,2,3,4,5]);
+          expect(data.key2).to.be(true);
+          expect(data.key3).to.be("testing");
+        });
+      });
+      describe("#parseFileData", function() {
+        it("should return an object", function(done){
+          var data = dg.parseFileData('{"version": "0.0.0", "saved_macs": [] }', function(err, data){
+            if (err)
+              expect().fail("Error parsing file ", err);
+            expect(data).to.be.an("object");
+            expect(data).to.eql({version: "0.1.13", saved_macs: []});
+            done();
+          });
+        });
+        it("should gracefully return null on bad input", function(){
+          var data = dg.parseFileData("{'name' 'test', 'saved_macs': []}", function(err, data){
+            expect(data).to.be(null);
+            expect(err).to.not.be(null);
+          });
+        });
+      });
+      describe("#fixOldFormat", function() {
 
-    });
-    describe("#updateItemTime", function() {
+      });
+      describe("#serialize", function() {
 
-    });
-    describe("#save", function() {
+      });
+      describe("#indexOfDev", function() {
 
+      });
+      describe("#deviceExists", function() {
+
+      });
+      describe("#addItem", function() {
+        var macs = dg.getItems();
+        expect(macs).to.have.length(0);
+        dg.addItem({
+          name: "test"
+        });
+        expect(dg.getItemByName("test")).to.be.an("object");
+        macs = dg.getItems();
+        expect(macs).to.have.length(1);
+        dg.removeItem("test");
+      });
+      describe("#removeItem", function() {
+
+      });
+      describe("#updateItemTime", function() {
+
+      });
+      describe("#save", function() {
+
+      });
     });
   });
 
