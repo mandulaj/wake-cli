cdd55c8a760419bc1028adc2efee4268165ccec5
Jakub Mandula <jakub.aludnam@gmail.com>
2014-12-07 16:21:31 +0100

    jshint + beautify

--- test/test.js
+++ test/test.js
@@ -1,6 +1,7 @@
 var expect = require("expect.js");
 var config = require("../config.json"); // the real config
 var basePath = __dirname;
+
 function clone(obj) {
   if (obj === null || typeof obj !== 'object')
     return obj;
@@ -816,30 +817,34 @@ describe("Helper Functions", function() {
     });
   });
 
-  describe("TermRender", function(){
+  describe("TermRender", function() {
     var conf = clone(config);
     conf.wakefile = "testWakefiles/wakefile5.json";
     var dg = require("../lib/dataGetter.js")(conf, basePath);
     var td = require("../lib/termRender.js")(dg);
-    describe("#constructor", function(){
-      it("should create an object with all the methods", function(){
-        expect(td).to.be.an("object")
+    describe("#constructor", function() {
+      it("should create an object with all the methods", function() {
+        expect(td).to.be.an("object");
       });
-      it("should have all the methods", function(){
+      it("should have all the methods", function() {
         expect(td.cloneArray).to.be.a("function");
         expect(td.sortList).to.be.a("function");
         expect(td.renderList).to.be.a("function");
       });
     });
 
-    describe("#cloneArray", function(){
-      it("should return an identical array of any value", function(){
-        var array = [0,"string",null, {a:0}, true, [1,2,3,4], function(x){return x*x}];
+    describe("#cloneArray", function() {
+      it("should return an identical array of any value", function() {
+        var array = [0, "string", null, {
+          a: 0
+        }, true, [1, 2, 3, 4], function(x) {
+          return x * x;
+        }];
         var copy_array = td.cloneArray(array);
         expect(copy_array).to.eql(array); // TODO: should really equal() not just eql()
       });
-      it("should return a clone", function(){
-        var array = [0,1,2,3,4,5,6,7,8,9,10];
+      it("should return a clone", function() {
+        var array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
         var array_clone = array;
         array_clone[0] = "changed";
         expect(array_clone).to.equal(array);
@@ -849,41 +854,79 @@ describe("Helper Functions", function() {
       });
     });
 
-    describe("#sortList", function(){
-      var array1 = [
-        {name:"a",mac:"666666666666",lastUsed: 15},
-        {name:"b",mac:"555555555555",lastUsed: 12},
-        {name:"c",mac:"444444444444",lastUsed: 13},
-        {name:"d",mac:"333333333333",lastUsed: 17},
-        {name:"e",mac:"222222222222",lastUsed: 20},
-        {name:"f",mac:"111111111111",lastUsed: 10}
-      ];
-      var array2 = [
-        {name:"f",mac:"111111111111",lastUsed: 10},
-        {name:"e",mac:"222222222222",lastUsed: 20},
-        {name:"d",mac:"333333333333",lastUsed: 17},
-        {name:"c",mac:"444444444444",lastUsed: 13},
-        {name:"b",mac:"555555555555",lastUsed: 12},
-        {name:"a",mac:"666666666666",lastUsed: 15}
-      ];
-      it("should sort the array of objects by the given name", function(){
+    describe("#sortList", function() {
+      var array1 = [{
+        name: "a",
+        mac: "666666666666",
+        lastUsed: 15
+      }, {
+        name: "b",
+        mac: "555555555555",
+        lastUsed: 12
+      }, {
+        name: "c",
+        mac: "444444444444",
+        lastUsed: 13
+      }, {
+        name: "d",
+        mac: "333333333333",
+        lastUsed: 17
+      }, {
+        name: "e",
+        mac: "222222222222",
+        lastUsed: 20
+      }, {
+        name: "f",
+        mac: "111111111111",
+        lastUsed: 10
+      }];
+      var array2 = [{
+        name: "f",
+        mac: "111111111111",
+        lastUsed: 10
+      }, {
+        name: "e",
+        mac: "222222222222",
+        lastUsed: 20
+      }, {
+        name: "d",
+        mac: "333333333333",
+        lastUsed: 17
+      }, {
+        name: "c",
+        mac: "444444444444",
+        lastUsed: 13
+      }, {
+        name: "b",
+        mac: "555555555555",
+        lastUsed: 12
+      }, {
+        name: "a",
+        mac: "666666666666",
+        lastUsed: 15
+      }];
+      it("should sort the array of objects by the given name", function() {
         var result = td.sortList("mac", false, array1);
         expect(result).to.eql(array2);
       });
-      it("should reverse the sort if asked to do so", function(){
+      it("should reverse the sort if asked to do so", function() {
         var result = td.sortList("mac", true, array2);
         expect(result).to.eql(array1);
       });
     });
 
-    describe("#renderList", function(){
+    describe("#renderList", function() {
       it("should return a string", function(done) {
         var conf = clone(config);
         conf.wakefile = "testWakefiles/wakefile5.json";
         var dg = require("../lib/dataGetter.js")(conf, basePath);
-        dg.getDataFromFile(function(){
+        dg.getDataFromFile(function() {
           var td = require("../lib/termRender.js")(dg);
-          dg.addItem({name:"test1",mac:"121212121212", lastUse: 0})
+          dg.addItem({
+            name: "test1",
+            mac: "121212121212",
+            lastUse: 0
+          });
           var result = td.renderList();
           expect(result).to.be.a("string");
           expect(result).to.match(/Name/);
@@ -892,25 +935,33 @@ describe("Helper Functions", function() {
           done();
         });
       });
-      it("should convert -Infinity to 'never'", function(done){
+      it("should convert -Infinity to 'never'", function(done) {
         var conf = clone(config);
         conf.wakefile = "testWakefiles/wakefile5.json";
         var dg = require("../lib/dataGetter.js")(conf, basePath);
-        dg.getDataFromFile(function(){
+        dg.getDataFromFile(function() {
           var td = require("../lib/termRender.js")(dg);
-          dg.addItem({name:"test1",mac:"121212121212", lastUse: -Infinity})
+          dg.addItem({
+            name: "test1",
+            mac: "121212121212",
+            lastUse: -Infinity
+          });
           var result = td.renderList();
           expect(result).to.match(/never/);
           done();
         });
       });
-      it("should beautify the Mac", function(done){
+      it("should beautify the Mac", function(done) {
         var conf = clone(config);
         conf.wakefile = "testWakefiles/wakefile5.json";
         var dg = require("../lib/dataGetter.js")(conf, basePath);
-        dg.getDataFromFile(function(){
+        dg.getDataFromFile(function() {
           var td = require("../lib/termRender.js")(dg);
-          dg.addItem({name:"test1",mac:"121212121212", lastUse: 0})
+          dg.addItem({
+            name: "test1",
+            mac: "121212121212",
+            lastUse: 0
+          });
           var result = td.renderList();
           expect(result).to.match(/12:12:12:12:12:12/);
           done();
